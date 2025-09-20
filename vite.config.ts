e2db261b0a47c1b27dd74e5a99e1f176b7916b96
zKOSMOSz <115968653+zKOSMOSz@users.noreply.github.com>
2025-09-20 10:07:54 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+export default {
+  base: '/repo-name/',  // Замените на имя вашего репозитория
+};
